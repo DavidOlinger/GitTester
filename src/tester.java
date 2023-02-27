3b7ff4370e9d8feeffe4ae7e284a118f569fbdb5
BenSmith1202 <smithba22@gcc.edu>
2023-02-27 13:50:57 -0500

    Added ASCII art to encourage consumer interest.

--- src/tester.java
+++ src/tester.java
@@ -1,5 +1,12 @@
 public class tester {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 3: Electric Beegalee");
+        System.out.println("   \\\\  //   ");
+        System.out.println("^-[|]--[|]-^");
+        System.out.println("     \\/    ");
+        System.out.println("   \\vvvv/  ");
+        System.out.println("    \\^^/  ");
+        System.out.println("   angri   ");
     }
+
 }
